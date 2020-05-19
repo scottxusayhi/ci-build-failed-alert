94f506609e2cb1f020c9789b319ef69fcdb08d81
xudi <xudi@k2data.com.cn>
2020-05-19 14:33:24 +0800

    update arduino program

--- arduino/ci-build-failed-alert.ino
+++ arduino/ci-build-failed-alert.ino
@@ -7,6 +7,11 @@ const String RED = "red";
 const String YELLOW = "yellow";
 const String GREEN = "green";
 
+const String C110 = "c110";
+const String C101 = "c101";
+const String C011 = "c011";
+const String C111 = "c111";
+
 //这里就是定义 开和关
 void setup()
 {
@@ -14,12 +19,13 @@ void setup()
   pinMode(redPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
+  Serial.println("Arduino is Ready!");
 }
 void loop()
 {
   if (Serial.available())
   {
-    String ans = Serial.readString();
+    String ans = Serial.readStringUntil('\r');
     Serial.println(ans);
     if (ans == RED)
     {
@@ -44,6 +50,30 @@ void loop()
       digitalWrite(redPin, LOW);
       digitalWrite(yellowPin, LOW);
       digitalWrite(greenPin, LOW);
+    }
+    else if (ans == C110)
+    {
+      digitalWrite(redPin, HIGH);
+      digitalWrite(yellowPin, HIGH);
+      digitalWrite(greenPin, LOW);
+    }
+    else if (ans == C101)
+    {
+      digitalWrite(redPin, HIGH);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(greenPin, HIGH);
+    }
+    else if (ans == C011)
+    {
+      digitalWrite(redPin, LOW);
+      digitalWrite(yellowPin, HIGH);
+      digitalWrite(greenPin, HIGH);
+    }
+    else if (ans == C111)
+    {
+      digitalWrite(redPin, HIGH);
+      digitalWrite(yellowPin, HIGH);
+      digitalWrite(greenPin, HIGH);
     }
     else
     {
